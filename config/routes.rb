1d62fad0bbc03f0f6c96cc760a46769dcf090226
EC2 Default User <ec2-user@ip-172-31-7-64.ec2.internal>
2022-06-27 06:19:33 +0000

    Finish ouyou4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  
+  resources :users, only: [:index,:show,:edit,:update]do
+    resource :relationships, only: [:create, :destroy, :followings, :followers]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
 
   get "home/about"=>"homes#about"
   root to: "homes#top"
